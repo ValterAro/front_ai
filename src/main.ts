eb9969e6c542c939b10505605c9423a9503a4e99
ValterAro <valteraro@hotmail.com>
2023-03-23 15:29:28 +0200

    Getting started, added Sites tab

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
+
+// font awesome stuff
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret, fas, faR } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+// font awesome stuff
+library.add(faUserSecret, fas, faR, far)
+createApp(App).component('font-awesome-icon', FontAwesomeIcon)
 
 createApp(App).use(router).mount('#app')
---
+++ src/views/AiSitesView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="aisites">
+    <SiteTable/>
+  </div>
+</template>
+
+<script>
+import SiteTable from "@/components/SiteTable.vue";
+
+export default {
+  name: "AiSites",
+  components: {SiteTable}
+}
+</script>
+
+
